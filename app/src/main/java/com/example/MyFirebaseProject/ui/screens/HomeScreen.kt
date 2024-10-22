994498f32eece0eb166bac8413a696ec72256f17
Dmitriy Sidorkin <dmitriysidorcool@gmail.com>
2024-10-23 00:00:45 +0300

    Add home screen

--- app/src/main/java/com/example/MyFirebaseProject/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/MyFirebaseProject/ui/screens/HomeScreen.kt
@@ -1,37 +1,56 @@
 package com.example.MyFirebaseProject.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.MyFirebaseProject.R
 import com.example.MyFirebaseProject.ui.theme.MyFirebaseProjectTheme
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyApp() {
     MyFirebaseProjectTheme {
         Scaffold(
-            bottomBar = { BottomNavigation() }
+            topBar = { TopAppBar(scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()) },
+            bottomBar = { BottomNavigation() },
         ) { padding ->
-            HomeScreen(Modifier.padding(padding))
+            HomeScreen(padding)
         }
     }
 }
 
 @Composable
-fun HomeScreen(padding: Any) {
+fun HomeScreen(padding: PaddingValues) {
     Column {
-        Text("Hello, user!")
+        UserCard(modifier = Modifier.padding(padding))
     }
 }
 
@@ -70,5 +89,92 @@ private fun BottomNavigation(modifier: Modifier = Modifier) {
             selected = false,
             onClick = {}
         )
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(
+                    text = stringResource(R.string.bottom_navigation_add)
+                )
+            },
+            selected = false,
+            onClick = {}
+        )
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Email,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(
+                    text = stringResource(R.string.bottom_navigation_chat)
+                )
+            },
+            selected = false,
+            onClick = {}
+        )
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Notifications,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(
+                    text = stringResource(R.string.bottom_navigation_notifications)
+                )
+            },
+            selected = false,
+            onClick = {}
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBar(scrollBehavior: TopAppBarScrollBehavior, modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        scrollBehavior = scrollBehavior,
+        title = {
+            Text(
+                text = stringResource(R.string.app_name),
+                style = MaterialTheme.typography.headlineSmall,
+            )
+        },
+        modifier = modifier
+    )
+}
+
+@Composable
+fun UserCard(modifier: Modifier) {
+    Surface(
+        shape = MaterialTheme.shapes.medium,
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.surfaceVariant
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(PaddingValues(10.dp)),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(R.drawable.user),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.size(80.dp)
+            )
+            Text(
+                text = "Hello, User!",
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(16.dp)
+            )
+        }
     }
 }
